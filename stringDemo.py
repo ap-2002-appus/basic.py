bfd1f43f10795dd2c7a4b9ac883df225186872b5
ap-2002-appus <ibinsiby4619@gmail.com>
2024-10-18 16:23:39 +0530

    Add files via upload

--- stringDemo.py
+++ stringDemo.py
@@ -1,8 +1,8 @@
-'''
 Authour:Ibin Siby
 Date:18-10-2024
 Discription:Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
-'''
+
+
 str_var="Hello, World!"
 str_pl=str_var[7:12]
 
