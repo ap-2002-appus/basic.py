6df57e4257404b0afc093750c09454b802af4993
ap-2002-appus <ibinsiby4619@gmail.com>
2024-10-18 15:07:08 +0530

    Add files via upload

---
+++ basic input.py
@@ -0,0 +1,11 @@
+'''
+Author:ibin siby
+Date:18-10-2024
+Discription:Python program that asks the user to input their name and age. After receiving the input, display a message that includes the user's name and age.
+Hello,  ibin ! You are  26 years old.
+
+'''
+
+k=str(input("Enter the name:"))
+h=int(input("Enter the age:"))
+print('Hello,',k ,'! You are ',h,'years old.')
