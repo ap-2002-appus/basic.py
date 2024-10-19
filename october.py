4b9904c678bed23187b320015c793089bbc3e24a
ap-2002-appus <ibinsiby4619@gmail.com>
2024-10-19 14:47:36 +0530

    Add files via upload

---
+++ october.py
@@ -0,0 +1,46 @@
+'''
+Author-Ibin siby
+date-19-10-2024
+Discription- Python program that performs the following tasks:
+
+    User Input:
+        Ask the user to enter two numbers, num1 and num2.
+        Ask the user to enter a third number, num3.
+
+    Addition:
+        Add the first two numbers (num1 and num2).
+        Print the sum in the format: "The sum of num1 and num2 is: result"
+
+    Subtraction:
+        Subtract num2 from num1.
+        Print the difference in the format: "The difference when num2 is subtracted from num1 is: result"
+
+    Multiplication:
+        Multiply num1 by num2.
+        Print the product in the format: "The product of num1 and num2 is: result"
+
+    Division:
+        Divide num1 by num2.
+        Print the quotient in the format: "The quotient when num1 is divided by num2 is: result"
+Modulus:
+
+    Find the remainder when num1 is divided by num2.
+    Print the remainder in the format: "The remainder when num1 is divided by num2 is: result"
+
+Combined Arithmetic Operation:
+
+    Perform the following combined operation:
+    result = (num1 + num2) * num3 / 2
+    Print the result in the format: "The result of (num1 + num2) * num3 / 2 is: result"
+'''
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+num3=int(input("Enter third number:"))
+result=(num1+num2)
+print("The sum 0f num1 and num2 is:",result)
+
+print("The difference when num2 is subtracted from num1 is:",num2-num1)
+print("The product of num1 and num2 is:",num1*num2)
+print("The quotient when num1 is divided by num2 is:",num1/num2)
+print("The remainder when num1 is divided by num2 is:",num1%num2)
+print("The result of (num1 + num2) * num3 / 2 is:", (num1 + num2) * num3 )
