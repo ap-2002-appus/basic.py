14419e0e3e5edbe5ecf1ade3f2ea156926553d34
ap-2002-appus <ibinsiby4619@gmail.com>
2024-10-25 14:23:12 +0530

    Add files via upload

---
+++ octobermonth.py
@@ -0,0 +1,16 @@
+'''
+Author:Ibin Siby
+Date :25-10-2024
+discription:find the largest of two numbers
+'''
+
+
+a=int(input("Enter the number:"))
+b=int(input("Enter the number"))
+
+
+if(a>b):
+    print(a,"is greater than",b)
+else:
+    print(b,"is greater than",a)
+print("the remaining statements")
