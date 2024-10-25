38d7d0a9aa3bb098607a353988278de9c5d096cc
ap-2002-appus <ibinsiby4619@gmail.com>
2024-10-25 14:39:17 +0530

    Add files via upload

---
+++ zeronumber.py
@@ -0,0 +1,15 @@
+'''
+Author:Ibin Siby
+Date :25-10-2024
+discription:python program for checking the given number is possitive or negative
+'''
+
+
+a=int(input("Enter the number:"))
+
+if(a>0):
+    print("The number is positive")
+elif(a<0):
+    print("The number is negative")
+else:
+    print("The number is zero")
