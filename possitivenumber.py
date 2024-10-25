2ef4682eb230b9192ae42ad6fc33c8fb6fb8693c
ap-2002-appus <ibinsiby4619@gmail.com>
2024-10-25 14:29:40 +0530

    Add files via upload

---
+++ possitivenumber.py
@@ -0,0 +1,16 @@
+'''
+Author:Ibin Siby
+Date :25-10-2024
+discription:python program for checking the given number is possitive or negative
+'''
+
+
+a=int(input("Enter the number:"))
+b=0
+
+
+if(a>0):
+    print("The number is positive")
+else:
+    print("The number is negative")
+print("the remaining statements")
