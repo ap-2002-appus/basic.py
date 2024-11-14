3997d5c15a004804ca2005f71e621d39c5b965e0
Ibin Siby <ibinsiby4619@gmail.com>
2024-11-14 21:49:35 +0530

    Update IBINday3.py

--- IBINday3.py
+++ IBINday3.py
@@ -12,9 +12,9 @@ Use if and if-else statements to print:
 age=int(input("Enter your age:"))
 if(age<13):
     print("You are a Child")
-elif(age>13 and age<19):
+elif(age>=13 and age<=19):
     print("You are a Teen")
-elif(age>20 and age<64):
+elif(age>=20 and age<=64):
     print("You are an Adult")
 else:
     print("You are a Senior")
