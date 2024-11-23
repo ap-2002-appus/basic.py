36a08c641192f4a31227d2b85f9c706d213c33a7
Ibin Siby <ibinsiby4619@gmail.com>
2024-11-23 14:05:49 +0530

    Update until_odd.py

--- until_odd.py
+++ until_odd.py
@@ -1,5 +1,11 @@
+'''
 Authour:Ibin Siby
-Discription:
+Discription:The while loop is useful for processing input repeatedly until a specific condition is met. In this task, you will:
+
+Use a while loop to continuously ask the user for a number.
+Check if the number is odd.
+Stop the loop and display a message when an odd number is entered.
+'''
 
 
 
