5bd9c7f22771b8132112f67b6183c38c2594d296
Ibin Siby <ibinsiby4619@gmail.com>
2024-11-30 16:03:49 +0530

    Add files via upload

---
+++ reverse_stringusing function.py
@@ -0,0 +1,9 @@
+'''
+Authour:Ibin Siby
+Discription:  Write a Python program to reverse a string.
+'''
+
+def reverse(str):
+    print("reverse of string is :",str[-1::-1])
+str= input("Enter string:")
+reverse (str)
